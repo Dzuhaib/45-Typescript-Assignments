abb8919d6693e0fdd28d9dc7f748513517470263
Zuhaib ahmed <54768399+bhattizuhaib@users.noreply.github.com>
2024-02-17 12:51:50 -0800

    Update invitees.js

--- 14-Invitees/invitees.js
+++ 14-Invitees/invitees.js
@@ -1,6 +1,6 @@
 "use strict";
 // Define an array of people to invite to dinner
-let guestList = ["Zuhaib", "Ahmed", "Talha"];
+let guestList = ["Imran Khan", "Nawaz Sharif", "Asif Ali Zardari"];
 // Print personalized invitations to each person
 for (let i = 0; i < guestList.length; i++) {
     console.log(`Dear ${guestList[i]},\nYou are cordially invited to dinner. It would be an honor to have you join us.\nSincerely, Bhatti`);
