ffaaa82f6b856747c1524c86a0c6671bad47f73d
Zuhaib ahmed <54768399+bhattizuhaib@users.noreply.github.com>
2024-02-17 11:24:55 -0800

    Add files via upload

---
+++ 02-Personal-Message/app.ts
@@ -0,0 +1,10 @@
+let personName :string = '';
+
+personName = prompt('what is your name') || '';
+
+if(personName !== null && personName !== '' ){
+    alert('Hello ${personName}, would you like to learn some Python today?')
+    }
+    else{
+        alert('You have to fill your name')
+    }
