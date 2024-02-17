10a54dda11d328e2791a07270a2d6f27da18492e
Zuhaib ahmed <54768399+bhattizuhaib@users.noreply.github.com>
2024-02-17 12:04:28 -0800

    Add files via upload

---
+++ 11-Names/Names.js
@@ -0,0 +1,7 @@
+"use strict";
+// Define an array of names
+let names = ["Zuhaib", "Afridi", "Shoaib", "Usman", "Eve"];
+// Print each person's name one at a time
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ 11-Names/Names.ts
@@ -0,0 +1,7 @@
+// Define an array of names
+let names: string[] = ["Zuhaib", "Afridi", "Shoaib", "Usman", "Eve"];
+
+// Print each person's name one at a time
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
