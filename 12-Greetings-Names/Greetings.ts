231b55baa185c51777a84beccb07050d8ff76046
Zuhaib ahmed <54768399+bhattizuhaib@users.noreply.github.com>
2024-02-17 12:09:06 -0800

    Add files via upload

---
+++ 12-Greetings-Names/Greetings.ts
@@ -0,0 +1,7 @@
+// Define an array of names
+let names: string[] = ["Zuhaib", "Usman", "Shaoib", "Shafqat", "Zeb"];
+
+// Print personalized greetings to each person
+for (let i = 0; i < names.length; i++) {
+    console.log(`Hello, ${names[i]}! Have a great day!`);
+}
