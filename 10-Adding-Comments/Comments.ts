3b3c1bcbb7ae6629f2e13c93ddc6e23bee53155b
Zuhaib ahmed <54768399+bhattizuhaib@users.noreply.github.com>
2024-02-17 11:43:09 -0800

    Add files via upload

---
+++ 10-Adding-Comments/Comments.ts
@@ -0,0 +1,5 @@
+// Program written by: Zuhaib Ahmed
+// Current date: 02/16/2024
+// This program will write Hello world!
+
+console.log('Hello world!')
---
+++ 10-Adding-Comments/Comments2.js
@@ -0,0 +1,5 @@
+"use strict";
+// Program written by: Zuhaib Ahmed
+// Current date: 02/16/2024
+// This program will add two digits.
+console.log(100 - 22);
