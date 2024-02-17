a68ed4fe7f26278b7a12e4c93d85aac6387e2b57
Zuhaib ahmed <54768399+bhattizuhaib@users.noreply.github.com>
2024-02-17 11:36:36 -0800

    Add files via upload

---
+++ 06-Stripping-Names/Stripping_names.js
@@ -0,0 +1,5 @@
+"use strict";
+const personName = '    \n\tZuhaib Ahmed\t\n';
+console.log(personName);
+const without_whitespace = personName.trim();
+console.log(without_whitespace);
---
+++ 06-Stripping-Names/Stripping_names.ts
@@ -0,0 +1,4 @@
+const personName :string = '    \n\tZuhaib Ahmed\t\n';
+console.log(personName);
+const without_whitespace :string = personName.trim();
+console.log(without_whitespace);
