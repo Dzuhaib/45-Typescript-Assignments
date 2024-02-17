b5d646f9a1ed6dc96b19a045639f0d016c5a5810
Zuhaib ahmed <54768399+bhattizuhaib@users.noreply.github.com>
2024-02-17 12:10:34 -0800

    Add files via upload

---
+++ 13-Transport/transport.js
@@ -0,0 +1,7 @@
+"use strict";
+// Define an array of favorite modes of transportation
+let favoriteTransportation = ["motorcycle", "car", "bicycle", "bus", "scooter"];
+// Print statements about each item in the list
+for (let i = 0; i < favoriteTransportation.length; i++) {
+    console.log(`I would like to own a ${favoriteTransportation[i]}.`);
+}
